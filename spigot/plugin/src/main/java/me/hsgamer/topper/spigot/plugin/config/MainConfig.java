d255d5270a7a7c820784a920f37b7b3cc3f07cb8
hsgamer <huynhqtienvtag@gmail.com>
2024-11-01 13:28:06 +0700

    change default storage type to flat

--- spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/config/MainConfig.java
+++ spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/config/MainConfig.java
@@ -47,10 +47,10 @@ public interface MainConfig {
     @ConfigPath(value = "storage-type")
     @Comment({
             "The type of storage the plugin will use to store the value",
-            "Available: YAML, JSON, SQLITE, MYSQL"
+            "Available: FLAT, YAML, JSON, SQLITE, MYSQL"
     })
     default String getStorageType() {
-        return "yaml";
+        return "flat";
     }
 
     void reloadConfig();

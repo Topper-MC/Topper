04c0958b7d00f7b086da997922a6b759d841f858
hsgamer <huynhqtienvtag@gmail.com>
2025-01-31 18:13:04 +0700

    customize separator in SimpleQueryContext

--- query/simple/src/main/java/me/hsgamer/topper/query/simple/SimpleQueryContext.java
+++ query/simple/src/main/java/me/hsgamer/topper/query/simple/SimpleQueryContext.java
@@ -3,6 +3,7 @@ package me.hsgamer.topper.query.simple;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public final class SimpleQueryContext implements SimpleQuery.Context {
     public final @NotNull String name;
@@ -15,23 +16,27 @@ public final class SimpleQueryContext implements SimpleQuery.Context {
         this.actionName = actionName;
     }
 
-    public static Optional<SimpleQueryContext> fromQuery(@NotNull String query, boolean singleName) {
+    public static Optional<SimpleQueryContext> fromQuery(@NotNull String query, boolean singleName, @NotNull String separator) {
         String name;
         String actionName;
         String args;
         if (singleName) {
-            String[] split = query.split(";", 2);
+            String[] split = query.split(Pattern.quote(separator), 2);
             name = "";
             actionName = split[0];
             args = split.length > 1 ? split[1] : "";
         } else {
-            String[] split = query.split(";", 3);
+            String[] split = query.split(Pattern.quote(separator), 3);
             if (split.length < 2) return Optional.empty();
             name = split[0];
             actionName = split[1];
             args = split.length > 2 ? split[2] : "";
         }
         return Optional.of(new SimpleQueryContext(name, args, actionName));
+    }
+
+    public static Optional<SimpleQueryContext> fromQuery(@NotNull String query, boolean singleName) {
+        return fromQuery(query, singleName, ";");
     }
 
     @Override

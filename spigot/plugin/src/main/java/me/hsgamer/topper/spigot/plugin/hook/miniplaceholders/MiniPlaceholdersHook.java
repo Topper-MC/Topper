81718413c7ccb5d9ed3b2ceba09aa9a9c22b2347
hsgamer <huynhqtienvtag@gmail.com>
2025-01-31 22:48:45 +0700

    apply global and player placeholders to MiniPlaceholdersHook

--- spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/hook/miniplaceholders/MiniPlaceholdersHook.java
+++ spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/hook/miniplaceholders/MiniPlaceholdersHook.java
@@ -6,7 +6,12 @@ import io.github.projectunified.minelib.plugin.base.Loadable;
 import me.hsgamer.topper.spigot.plugin.TopperPlugin;
 import me.hsgamer.topper.spigot.plugin.builder.ValueProviderBuilder;
 import me.hsgamer.topper.spigot.plugin.manager.TopQueryManager;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiFunction;
 
 public class MiniPlaceholdersHook implements Loadable {
     private final TopperPlugin plugin;
@@ -14,18 +19,21 @@ public class MiniPlaceholdersHook implements Loadable {
 
     public MiniPlaceholdersHook(TopperPlugin plugin) {
         this.plugin = plugin;
+
+        BiFunction<@Nullable Player, ArgumentQueue, Tag> queryFunction = (player, queue) -> {
+            String query = queue.popOr("You need to specify the query").value();
+            String result = plugin.get(TopQueryManager.class).get(player, query);
+            if (result == null) {
+                return TagsUtils.EMPTY_TAG;
+            } else {
+                return TagsUtils.staticTag(result);
+            }
+        };
+
         this.expansion = Expansion.builder("topper")
+                .globalPlaceholder("global", (queue, context) -> queryFunction.apply(null, queue))
                 .filter(Player.class)
-                .audiencePlaceholder("query", (audience, queue, ctx) -> {
-                    Player player = (Player) audience;
-                    String query = queue.popOr("You need to specify the query").value();
-                    String result = plugin.get(TopQueryManager.class).get(player, query);
-                    if (result == null) {
-                        return TagsUtils.EMPTY_TAG;
-                    } else {
-                        return TagsUtils.staticTag(result);
-                    }
-                })
+                .audiencePlaceholder("player", (audience, queue, ctx) -> queryFunction.apply((Player) audience, queue))
                 .build();
     }
 

db11919ec579733a62a6b398a7db48a306c44380
hsgamer <huynhqtienvtag@gmail.com>
2025-02-01 18:10:36 +0700

    add advanced decimal and time format to ValueDisplay

--- spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/holder/display/ValueDisplay.java
+++ spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/holder/display/ValueDisplay.java
@@ -2,20 +2,27 @@ package me.hsgamer.topper.spigot.plugin.holder.display;
 
 import me.hsgamer.topper.query.simple.SimpleQueryDisplay;
 import me.hsgamer.topper.spigot.plugin.holder.NumberTopHolder;
+import org.apache.commons.lang.time.DurationFormatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.text.DecimalFormatSymbols;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ValueDisplay implements SimpleQueryDisplay<UUID, Double> {
     private static final Pattern VALUE_PLACEHOLDER_PATTERN = Pattern.compile("\\{value(?:_(.*))?}");
+    private static final String FORMAT_QUERY_DECIMAL_FORMAT_PREFIX = "decimal:";
+    private static final String FORMAT_QUERY_TIME_FORMAT_PREFIX = "time:";
 
     public final String line;
     public final String displayNullName;
@@ -37,6 +44,19 @@ public class ValueDisplay implements SimpleQueryDisplay<UUID, Double> {
                 .orElse("---");
     }
 
+    private static Map<String, String> getSettings(String query) {
+        if (query.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        final String separator = "&";
+        final String keyValueSeparator = "=";
+        return Arrays.stream(query.split(Pattern.quote(separator)))
+                .map(s -> s.split(Pattern.quote(keyValueSeparator), 2))
+                .filter(a -> a.length == 2)
+                .collect(Collectors.toMap(a -> a[0], a -> a[1], (a, b) -> a));
+    }
+
     public @NotNull String getDisplayKey(@Nullable UUID uuid) {
         return uuid != null ? uuid.toString() : displayNullUuid;
     }
@@ -48,22 +68,81 @@ public class ValueDisplay implements SimpleQueryDisplay<UUID, Double> {
                 .orElse(displayNullName);
     }
 
-    public @NotNull String getDisplayValue(@Nullable Double value, @Nullable String formatType) {
+    public @NotNull String getDisplayValue(@Nullable Double value, @Nullable String formatQuery) {
         if (value == null) {
             return displayNullValue;
         }
 
-        if (formatType == null) {
+        if (formatQuery == null) {
             DecimalFormat decimalFormat = new DecimalFormat("#.##");
             return decimalFormat.format(value);
         }
 
-        if (formatType.equals("raw")) {
+        if (formatQuery.equals("raw")) {
             return String.valueOf(value);
         }
 
+        if (formatQuery.startsWith(FORMAT_QUERY_DECIMAL_FORMAT_PREFIX)) {
+            Map<String, String> settings = getSettings(formatQuery.substring(FORMAT_QUERY_DECIMAL_FORMAT_PREFIX.length()));
+
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+            Optional.ofNullable(settings.get("decimalSeparator"))
+                    .map(s -> s.charAt(0))
+                    .ifPresent(symbols::setDecimalSeparator);
+            Optional.ofNullable(settings.get("groupingSeparator"))
+                    .map(s -> s.charAt(0))
+                    .ifPresent(symbols::setGroupingSeparator);
+
+            DecimalFormat decimalFormat = new DecimalFormat();
+            decimalFormat.setDecimalFormatSymbols(symbols);
+            decimalFormat.setRoundingMode(RoundingMode.HALF_EVEN);
+            Optional.ofNullable(settings.get("maximumFractionDigits"))
+                    .map(Integer::parseInt)
+                    .ifPresent(decimalFormat::setMaximumFractionDigits);
+            return decimalFormat.format(value);
+        }
+
+        if (formatQuery.startsWith(FORMAT_QUERY_TIME_FORMAT_PREFIX)) {
+            Map<String, String> settings = getSettings(formatQuery.substring(FORMAT_QUERY_TIME_FORMAT_PREFIX.length()));
+
+            String pattern = Optional.ofNullable(settings.get("pattern")).orElse("HH:mm:ss");
+            String type = Optional.ofNullable(settings.get("type")).orElse("duration");
+            long time = value.longValue();
+            TimeUnit unit = Optional.ofNullable(settings.get("unit"))
+                    .map(s -> {
+                        try {
+                            return TimeUnit.valueOf(s.toUpperCase());
+                        } catch (IllegalArgumentException e) {
+                            return null;
+                        }
+                    })
+                    .orElse(TimeUnit.SECONDS);
+            long millis = unit.toMillis(time);
+
+            if (type.equalsIgnoreCase("time")) {
+                DateTimeFormatter formatter;
+                try {
+                    formatter = DateTimeFormatter.ofPattern(pattern);
+                } catch (IllegalArgumentException e) {
+                    return "INVALID_FORMAT";
+                }
+                Instant date = Instant.ofEpochMilli(millis);
+                try {
+                    return formatter.format(date);
+                } catch (IllegalArgumentException e) {
+                    return "CANNOT_FORMAT";
+                }
+            } else if (type.equalsIgnoreCase("duration")) {
+                try {
+                    return DurationFormatUtils.formatDuration(millis, pattern);
+                } catch (IllegalArgumentException e) {
+                    return "INVALID_FORMAT";
+                }
+            }
+        }
+
         try {
-            DecimalFormat decimalFormat = new DecimalFormat(formatType);
+            DecimalFormat decimalFormat = new DecimalFormat(formatQuery);
             return decimalFormat.format(value);
         } catch (IllegalArgumentException e) {
             return "INVALID_FORMAT";

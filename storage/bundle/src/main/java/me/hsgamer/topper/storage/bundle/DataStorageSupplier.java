9743cfee7d65d85372ff77075763619e08868d5e
hsgamer <huynhqtienvtag@gmail.com>
2025-09-18 23:26:32 +0700

    remove bundle module
    not needed

--- storage/bundle/src/main/java/me/hsgamer/topper/storage/bundle/DataStorageSupplier.java
+++
@@ -1,7 +0,0 @@
-package me.hsgamer.topper.storage.bundle;
-
-import me.hsgamer.topper.storage.core.DataStorage;
-
-public interface DataStorageSupplier {
-    <K, V> DataStorage<K, V> getStorage(String name, ValueConverter<K, V> valueConverter);
-}

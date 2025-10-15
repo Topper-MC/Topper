368eb967bfdbd4e40353e672cfbd0521b23c9ef8
hsgamer <huynhqtienvtag@gmail.com>
2025-10-15 22:58:50 +0700

    DataStorageSupplier: get storage supplier, given the converters

--- template/storage-supplier/src/main/java/me/hsgamer/topper/template/storagesupplier/storage/DataStorageSupplier.java
+++ template/storage-supplier/src/main/java/me/hsgamer/topper/template/storagesupplier/storage/DataStorageSupplier.java
@@ -4,6 +4,8 @@ import me.hsgamer.topper.storage.core.DataStorage;
 import me.hsgamer.topper.storage.flat.core.FlatValueConverter;
 import me.hsgamer.topper.storage.sql.core.SqlValueConverter;
 
+import java.util.function.Function;
+
 public interface DataStorageSupplier {
     <K, V> DataStorage<K, V> getStorage(
             String name,
@@ -12,4 +14,13 @@ public interface DataStorageSupplier {
             SqlValueConverter<K> sqlKeyConverter,
             SqlValueConverter<V> sqlValueConverter
     );
+
+    default <K, V> Function<String, DataStorage<K, V>> getStorageSupplier(
+            FlatValueConverter<K> keyConverter,
+            FlatValueConverter<V> valueConverter,
+            SqlValueConverter<K> sqlKeyConverter,
+            SqlValueConverter<V> sqlValueConverter
+    ) {
+        return name -> getStorage(name, keyConverter, valueConverter, sqlKeyConverter, sqlValueConverter);
+    }
 }

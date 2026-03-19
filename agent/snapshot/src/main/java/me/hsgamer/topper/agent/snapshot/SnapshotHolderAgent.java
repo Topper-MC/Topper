f30adc1552ac1b075833c5f5f14fa1506bf83b6b
hsgamer <huynhqtienvtag@gmail.com>
2026-03-19 22:01:07 +0700

    add SnapshotHolderAgent

---
+++ agent/snapshot/src/main/java/me/hsgamer/topper/agent/snapshot/SnapshotHolderAgent.java
@@ -0,0 +1,52 @@
+package me.hsgamer.topper.agent.snapshot;
+
+import me.hsgamer.topper.agent.core.DataEntryAgent;
+import me.hsgamer.topper.data.core.DataEntry;
+import me.hsgamer.topper.data.core.DataHolder;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+public class SnapshotHolderAgent<K, V> extends SnapshotAgent<K, V> implements DataEntryAgent<K, V> {
+    private final DataHolder<K, V> holder;
+    private final AtomicBoolean needUpdating = new AtomicBoolean(true);
+
+    public SnapshotHolderAgent(DataHolder<K, V> holder) {
+        this.holder = holder;
+    }
+
+    @Override
+    protected Stream<Map.Entry<K, V>> getDataStream() {
+        return holder.getEntryMap()
+                .entrySet()
+                .stream()
+                .map(entry -> new AbstractMap.SimpleImmutableEntry<>(entry.getKey(), entry.getValue().getValue()));
+    }
+
+    @Override
+    protected boolean needUpdate() {
+        if (needUpdating.get()) {
+            needUpdating.set(false);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onCreate(DataEntry<K, V> entry) {
+        needUpdating.set(true);
+    }
+
+    @Override
+    public void onUpdate(DataEntry<K, V> entry, V oldValue, V newValue) {
+        needUpdating.set(true);
+    }
+
+    @Override
+    public void onRemove(DataEntry<K, V> entry) {
+        needUpdating.set(true);
+    }
+}
